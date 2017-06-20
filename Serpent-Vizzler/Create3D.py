c849639454db537ebe7545fb0a0bbbeff4949a4b
Terryphi <terry.price@gmail.com>
2017-06-20 13:17:02 -0400

    Updated

--- Serpent-Vizzler/Create3D.py
+++ Serpent-Vizzler/Create3D.py
@@ -1,82 +1,79 @@
 %pwd
 %cd /home/robot2/Documents/SSS/c757mnyws00/Sandbox/3D-Gen/images/
-
+%cd ..
 import os
 from natsort import natsort_keygen, ns, natsorted
-
-#FILE LIST
-#get the image list.
-#DEPTHS LIST
-fileListS1 = os.listdir()
-fileListS2 = [x for x in fileListS1 if 'png' in x and 'vizzle' in x]
-#sort by number.
-fileListS3 = natsorted(fileListS2, key = lambda y: y.lower());
-
-f = open('../vizzleDepths','r')
-depthsS1 = f.readlines()
-f.close()
-depthsS2 = [x.rstrip() for x in depthsS1]
-depthsS3 = [float(x) for x in depthsS2]
-
-
-#depths with files.
-depthFileL = list(zip(depthsS3,fileListS3))
-
-#RGB COLOR DATA
-#load the card RGB Data
-with open('../cardRGBData','r') as f: rgbDataS1 = [ x.rstrip().split() for x in f.readlines()]
-rgbDataS2 = [(tuple(map(int,tuple(x[0:3]))),x[3]) for x in rgbDataS1]
-#the first element is the scalar value to pass into paraview.
-rgbDataS3 = [(idx+1,) + x for idx,x in enumerate(rgbDataS2)]
-rgbDataS4 = [(0,(0,0,0), 'void')] + rgbDataS3 #could of prepended back in the
-
-
-
-#...genImages stage...but w/e
-
 from PIL import Image
 import itertools as IT
 
-#write a new csv for each mat-num.
-#matNums = [x[0] for x in rgbDataS4]
-#for m in matNums:#
-#    toWriteS1 = [x for x in outputS1 if x[1] == m]
-#    toWriteS2 = ['{0},{1},{2},{3}\n'.format(*x[0],depth,x[1]) for x in toWriteS1]
-#    with open(r'vizzleOutput{0}.csv'.format(m),'w') as f:
-#        f.writelines('X-COORD,Y-COORD,Z-COORD,MAT-NUM\n')
-
-
-bbSize = 100 #same as the BB in the input file.
+bbSize = -7*2
+ #same as the BB in the input file.
 imSize = 200
 scaleFactor = bbSize/ float(imSize) #(units per pixel)
 
-matNums = [x[0] for x in rgbDataS4]
-output = {}
-output['total'] = []
+gen3DData()
+
+def gen3DData():
+    %cd ./images
+    #FILE LIST
+    #get the image list.
+    fileListS1 = os.listdir()
+    fileListS2 = [x for x in fileListS1 if 'png' in x and 'vizzle' in x]
+    #sort by number.
+    fileListS3 = natsorted(fileListS2, key = lambda y: y.lower());
+
+    %cd ..
+    f = open('vizzleDepths','r')
+    depthsS1 = f.readlines()
+    f.close()
+    depthsS2 = [x.rstrip() for x in depthsS1]
+    depthsS3 = [float(x) for x in depthsS2]
+
+    #depths with files.
+    depthFileL = list(zip(depthsS3,fileListS3))
+
+    #RGB COLOR DATA
+    #load the card RGB Data
+    with open('cardRGBData','r') as f: rgbDataS1 = [ x.rstrip().split() for x in f.readlines()]
+    rgbDataS2 = [(tuple(map(int,tuple(x[0:3]))),x[3]) for x in rgbDataS1]
+    #the first element is the scalar value to pass into paraview.
+    rgbDataS3 = [(idx+1,) + x for idx,x in enumerate(rgbDataS2)]
+    rgbDataS4 = [(0,(0,0,0), 'void')] + rgbDataS3 #could of prepended back in the
 
 
-for m in matNums: output[m] = []
-for d,f in depthFileL:
-    print('working on image {0}'.format(f))
-    im = Image.open(f)
-    imRGB = im.convert('RGB')
-    pixels = list(IT.product(range(imSize),range(imSize)))
-    #stage 1 is ((X,Y),SCALAR)
-    outputS1 = []
-    for p in pixels:
-        color = imRGB.getpixel(p)
-        for mat in rgbDataS4:
-            if color == mat[1]:
-                outputS1.append((p,mat[0]))
-    #Stage 2 output is ready to write.
-    outputS2 = ['{0},{1},{2},{3}\n'.format(x[0][0]*scaleFactor,x[0][1]*scaleFactor,d,x[1]) for x in outputS1]
-    output['total'].append(outputS2)
+    #write a new csv for each mat-num.
+    #matNums = [x[0] for x in rgbDataS4]
+    #for m in matNums:#
+    #    toWriteS1 = [x for x in outputS1 if x[1] == m]
+    #    toWriteS2 = ['{0},{1},{2},{3}\n'.format(*x[0],depth,x[1]) for x in toWriteS1]
+    #    with open(r'vizzleOutput{0}.csv'.format(m),'w') as f:
+    #        f.writelines('X-COORD,Y-COORD,Z-COORD,MAT-NUM\n')
 
 
-%pwd
-%cd ..
-with open(r'vizzleOutputTot.csv','w') as f:
-    f.writelines('X-COORD,Y-COORD,Z-COORD,MAT-NUM\n')
-    for idx, s in enumerate(output['total']):
-        print('on idx {0}'.format(idx))
-        f.writelines(s)
+    %cd ./images
+
+    matNums = [x[0] for x in rgbDataS4]
+    output = {}
+    output['total'] = []
+    for m in matNums: output[m] = []
+    for d,f in depthFileL:
+        print('working on image {0}'.format(f))
+        im = Image.open(f)
+        imRGB = im.convert('RGB')
+        pixels = list(IT.product(range(imSize),range(imSize)))
+        #stage 1 is ((X,Y),SCALAR)
+        outputS1 = []
+        for p in pixels:
+            color = imRGB.getpixel(p)
+            for mat in rgbDataS4:
+                if color == mat[1]:
+                    outputS1.append((p,mat[0]))
+        #Stage 2 output is ready to write.
+        outputS2 = ['{0},{1},{2},{3}\n'.format(x[0][0]*scaleFactor,x[0][1]*scaleFactor,d,x[1]) for x in outputS1]
+        output['total'].append(outputS2)
+
+    with open(r'../vizzleOutputTot.csv','w') as f:
+        f.writelines('X-COORD,Y-COORD,Z-COORD,MAT-NUM\n')
+        for idx, s in enumerate(output['total']):
+            print('on idx {0}'.format(idx))
+            f.writelines(s)
--- Serpent-Vizzler/genImages.py
+++ Serpent-Vizzler/genImages.py
@@ -4,60 +4,74 @@ import numpy as np
 import os
 import psutil
 import shutil
+import random
 
-sssPath = r'/home/robot2/Documents/SSS/c757mnyws00/sss2'
-fileDir = r'/home/robot2/Documents/SSS/c757mnyws00/Sandbox/3D-Gen/vizzleFile'
-
-
-
-bounds = (-51,51)
-slices = 100
-size = 200
-
-f = open('./vizzleDepths','w')
-output = []
-for p in list(np.linspace(*bounds,slices)):
-    output.append('plot 1 {0} {0} {1}'.format(size, p))
-    f.write(str(p) + '\n')
-
-f.close()
+#for the lazy
+r = lambda : random.randint(0,255)
+['rgb {0},{1},{2}'.format(r(),r(),r()) for x in range(15)]
+bbSize = -7*2
+fileName = './bwr'
+#NB Do three things:
+#1) make sure you put in %VIZZLE into the file.
+#2) make sure all the MAT cards have rgb tags at the end of them
+#---and each MAT should have a unique RGB value.
+#3) make sure there are no other plot commands in the input file.
+genTheImages(bounds,fileName)
 
 
-f = open('./Unit1','r')
-fileData = f.readlines()
-f.close()
+def genTheImages(bounds, fileName):
+    vizzleFileName = 'vizzleFile'
+    sssPath = r'/home/robot2/Documents/SSS/c757mnyws00/sss2'
+    fileDir = r'/home/robot2/Documents/SSS/c757mnyws00/Sandbox/3D-Gen/' + vizzleFileName
+    #don't change this^
 
-#first, gen the cell card data.
-matLines = [x for x in fileData if 'mat' in x]
-rgbData = []
-for l in matLines:
-    tokens = l.split()
-    name = tokens[1]
-    vals = []
-    #get the index of the rgb token.
-    for idx, t in enumerate(tokens):
-        if t == 'rgb':
-             vals = tokens[idx+1:] #extractthe rgb data.
-    rgbData.append('{0} {1} {2} {3}\n'.format(*vals,name))
+    #CHANGE SETTINGS HERE
+    slices = 100
+    size = 200 #size of plot
 
-with open('cardRGBData','w') as f:
-    f.writelines(rgbData)
+    f = open('./vizzleDepths','w')
+    output = []
+    for p in list(np.linspace(-bbSize/2,bbSize/2,slices)):
+        output.append('plot 1 {0} {0} {1}'.format(size, p))
+        f.write(str(p) + '\n')
+    f.close()
 
 
-insertAt = [idx for idx, s in enumerate(fileData) if s.startswith('%VIZZLE')][0]
-for s in reversed(output):
-    fileData.insert(insertAt,s)
+    f = open(fileName,'r')
+    fileData = f.readlines()
+    f.close()
+
+    #first, gen the cell card data.
+    matLines = [x for x in fileData if 'mat ' in x]
+    rgbData = []
+    for l in matLines:
+        tokens = l.split()
+        name = tokens[1]
+        vals = []
+        tokens
+        #get the index of the rgb token.
+        for idx, t in enumerate(tokens):
+            if t == 'rgb':
+                 vals = tokens[idx+1:] #extractthe rgb data.
+        rgbData.append('{0} {1} {2} {3}\n'.format(*vals,name))
+
+    with open('cardRGBData','w') as f:
+        f.writelines(rgbData)
+
+    insertAt = [idx for idx, s in enumerate(fileData) if s.startswith('%VIZZLE')][0]
+    for s in reversed(output):
+        fileData.insert(insertAt,s)
 
 
-f = open('./vizzleFile', 'w')
-f.writelines([x + '\n' for x in fileData])
-f.close()
+    f = open('./vizzleFile', 'w')
+    f.writelines([x + '\n' for x in fileData])
+    f.close()
 
-termLine = '{0} -plot {1}'.format(sssPath,fileDir)
-os.system(termLine)
+    termLine = '{0} -plot {1}'.format(sssPath,fileDir)
+    os.system(termLine)
 
-#move the files.
-moveList = [x for x in os.listdir() if 'vizzleFile_geom' in x]
+    #move the files.
+    moveList = [x for x in os.listdir() if 'vizzleFile_geom' in x]
 
-for f in moveList:
-    shutil.move(f, './images/' + f)
+    for f in moveList:
+        shutil.move(f, './images/' + f)
